f8a864b9f8f0f187850c8cbb3b02eb94ff0dcd1e
thanvinhbaohoang <thanvinhbaohoang@gmail.com>
2023-09-21 19:04:16 -0400

    emmet

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const FilterButtons = () => {
   return (
     <div className='flex gap-2'>
-            <button className="border-2 p-2  rounded-lg"> Rejection Types </button>
-            <button className="border-2 px-2 rounded-lg"> Claim Status </button>
-           </div>
+      <button className="border-2 p-2  rounded-lg"> Rejection Types </button>
+      <button className="border-2 px-2 rounded-lg"> Claim Status </button>
+      </div>
   )
 }
